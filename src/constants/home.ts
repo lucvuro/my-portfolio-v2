a8bb4601013c1922c43f2e16ff662dfec91e5460
MAC <vu.luc@genkin.com.vn>
2025-02-08 17:12:41 +0700

    feat: add loading

---
+++ src/constants/home.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_LOADING_VALUE = 0;
--- src/features/pages/home/index.tsx
+++ src/features/pages/home/index.tsx
@@ -1,5 +1,38 @@
+"use client";
+import { AppConfig } from "@/configs/app";
+import { DEFAULT_LOADING_VALUE } from "@/constants/home";
+import { Loading } from "@/features/pages/home/components";
+import { isBrowser } from "@/libs/ssr";
+import { useEffect, useState } from "react";
+
 const HomePage = () => {
-  return <></>;
+  const [loadingValue, setLoadingValue] = useState(DEFAULT_LOADING_VALUE);
+
+  useEffect(() => {
+    if (isBrowser) {
+      const interval = setInterval(() => {
+        setLoadingValue((prev) => {
+          if (prev >= AppConfig.MAX_LOADING_VALUE) {
+            clearInterval(interval);
+            return AppConfig.MAX_LOADING_VALUE;
+          }
+          return prev + 1;
+        });
+      }, 20);
+
+      return () => clearInterval(interval);
+    }
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      {loadingValue < AppConfig.MAX_LOADING_VALUE ? (
+        <Loading value={loadingValue} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
