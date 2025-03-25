58af07a092a566f5a9d4fd6f07e9e5552b7e692d
MAC <vu.luc@genkin.com.vn>
2025-03-25 12:30:33 +0700

    feat: update avatar picture

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,4 @@
-export { default as avatarPixel } from "./avatar-pixel-arrt.png";
+export { default as avatarPixel } from "./caubedo.jpg";
 export { default as stickTogetherImg } from "./sticktogether.jpg";
 export { default as ecommerProjectImg } from "./ecommerce.png";
 export { default as porfolioImg } from "./portfolio.png";
